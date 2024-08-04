f01ec3da895d2756056ecb04d2734ac83179cac7
jesusmontilla03 <jsdmontilla@gmail.com>
2024-08-03 22:09:35 -0400

    escuchando eventos

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const clase = document.getElementsByClassName('one');
-const identificador = document.getElementById('two');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular')
+const resultado = document.querySelector('#result');
 
-console.log(input.value);
-
-console.log({
-    h1,
-    p,
-    clase,
-    identificador,
-    input
-});
-
-h1.innerText = 'EXELCIOR'
+function btnOnClick () {
+    const sumaResult = input1.value + input2.value;
+    resultado.innerText = "El Resultado es: " + sumaResult;
+}
