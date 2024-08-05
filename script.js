123c1bec810756031aeb1fd9adc8ecc068992b0d
jesusmontilla03 <jsdmontilla@gmail.com>
2024-08-04 21:56:17 -0400

    addEventListener

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular')
 const resultado = document.querySelector('#result');
 
-function btnOnClick () {
+// form.addEventListener('submit', sumarInputValues);
+btn.addEventListener('click', sumarInputValues);
+
+function sumarInputValues () {
+    // console.log(event);
+    // event.preventDefault();
     const sumaResult = input1.value + input2.value;
     resultado.innerText = "El Resultado es: " + sumaResult;
 }
